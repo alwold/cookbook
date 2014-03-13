8cb5431901edbb0159dd5c190a7655a350d3416b
Al Wold <alwold@alwold.com>
2014-03-12 21:12:19 -0700

    - add nice looking names to the index - show the name above the recipe - clear out the recipe from the scope if there's an error

--- app/js/cookbook.js
+++ app/js/cookbook.js
@@ -4,13 +4,16 @@ cookbookApp.controller('CookbookCtrl', function ($scope, $http) {
   $http.get('data/index.json').success(function(data) {
     $scope.recipes = data;
   });
-  $scope.view = function(name) {
-    $http.get('data/'+name+'.json')
+  $scope.view = function(file, name) {
+    $http.get('data/'+file+'.json')
       .success(function(data) {
         $scope.recipe = data;
+        $scope.name = name;
       })
       .error(function() {
         alert('Error loading recipe');
+        delete $scope.recipe;
+        delete $scope.name;
       });
   }
 });
