c0528aefb4231de37e00ea6b794f49757b9dd127
Al Wold <alwold@alwold.com>
2014-03-12 21:04:30 -0700

    add index of recipes and allow clicking to load

--- app/js/cookbook.js
+++ app/js/cookbook.js
@@ -1,7 +1,16 @@
 var cookbookApp = angular.module('cookbookApp', []);
 
 cookbookApp.controller('CookbookCtrl', function ($scope, $http) {
-  $http.get('data/shrimp-burgers.json').success(function(data) {
-    $scope.recipe = data;
+  $http.get('data/index.json').success(function(data) {
+    $scope.recipes = data;
   });
+  $scope.view = function(name) {
+    $http.get('data/'+name+'.json')
+      .success(function(data) {
+        $scope.recipe = data;
+      })
+      .error(function() {
+        alert('Error loading recipe');
+      });
+  }
 });
