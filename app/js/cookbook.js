af5339412f186aab161192528c4b076c1feb1cbf
Al Wold <alwold@alwold.com>
2014-03-13 11:31:34 -0700

    rework the fraction stuff so that it keeps the whole part of the number preserved

--- app/js/cookbook.js
+++ app/js/cookbook.js
@@ -19,22 +19,33 @@ cookbookApp.controller('CookbookCtrl', function ($scope, $http) {
   $scope.formatAmount = function(amount) {
     if (typeof(amount) === 'number') {
       // see if it converts to a fraction
+      if (amount > 1) {
+        var whole = Math.floor(amount);
+        var fraction = amount - whole;
+      } else {
+        var fraction = amount;
+      }
       if (amount === Math.floor(amount)) {
         return amount;
-      } else if (amount * 2 === Math.floor(amount * 2)) {
-        return amount * 2 + '/2';
-      } else if (String(amount).indexOf('0.333') === 0) {
-        return '1/3';
-      } else if (String(amount).indexOf('0.66') === 0) {
-        return '2/3';
-      } else if (amount * 4 === Math.floor(amount * 4)) {
-        return amount * 4 + '/4';
-      } else if (amount * 6 === Math.floor(amount * 6)) {
-        return amount * 6 + '/6';
-      } else if (amount * 8 === Math.floor(amount * 8)) {
-        return amount * 8 + '/8';
+      } else if (fraction * 2 === Math.floor(fraction * 2)) {
+        var formattedFraction = fraction * 2 + '/2';
+      } else if (String(fraction).indexOf('0.333') === 0) {
+        var formattedFraction = '1/3';
+      } else if (String(fraction).indexOf('0.66') === 0) {
+        var formattedFraction = '2/3';
+      } else if (fraction * 4 === Math.floor(fraction * 4)) {
+        var formattedFraction = fraction * 4 + '/4';
+      } else if (fraction * 6 === Math.floor(fraction * 6)) {
+        var formattedFraction = fraction * 6 + '/6';
+      } else if (fraction * 8 === Math.floor(fraction * 8)) {
+        var formattedFraction = fraction * 8 + '/8';
       } else {
         return amount;
+      }
+      if (whole) {
+        return whole + ' ' + formattedFraction;
+      } else {
+        return formattedFraction;
       }
     } else {
       return amount;
