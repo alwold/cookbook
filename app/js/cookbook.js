f44e4089b579665a6b2e2a9c63a4d422359f0616
Al Wold <alwold@alwold.com>
2014-03-12 22:21:01 -0700

    add hack to deal with thirds

--- app/js/cookbook.js
+++ app/js/cookbook.js
@@ -21,16 +21,16 @@ cookbookApp.controller('CookbookCtrl', function ($scope, $http) {
       // see if it converts to a fraction
       if (amount === Math.floor(amount)) {
         return amount;
-      } else if (amount * 2 === Math.floor(amount * 2)) {
-        return amount * 2 + "/2";
-      } else if (amount * 3 === Math.floor(amount * 3)) {
-        return amount * 3 + "/3";
+      } else if (String(amount).indexOf('0.333') === 0) {
+        return '1/3';
+      } else if (String(amount).indexOf('0.66') === 0) {
+        return '2/3';
       } else if (amount * 4 === Math.floor(amount * 4)) {
-        return amount * 4 + "/4";
+        return amount * 4 + '/4';
       } else if (amount * 6 === Math.floor(amount * 6)) {
-        return amount * 6 + "/6";
+        return amount * 6 + '/6';
       } else if (amount * 8 === Math.floor(amount * 8)) {
-        return amount * 8 + "/8";
+        return amount * 8 + '/8';
       } else {
         return amount;
       }
